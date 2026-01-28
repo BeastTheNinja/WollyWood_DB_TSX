649a9d952b6e7e53ec67c329bd142dbdc2eb2d1a
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-28 09:58:42 +0100

    removed custom variable

--- frontend/src/Layout/Layout.tsx
+++ frontend/src/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Footer } from "../components/Footer/Footer"
 
 export const Layout = () => {
     return (
-        <div className="max-w-[1366px]  mx-auto">
+        <div className="max-w-341.5  mx-auto">
             <div className="bg-[#F9F9F9]  shadow-lg">
                 <Header />
                 <Outlet />
