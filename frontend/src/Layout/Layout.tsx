e5f5ecea93e3b8f2ec4881c3d9037bfc682f1b0c
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-02-02 08:38:24 +0100

    added max height on screen to match design

--- frontend/src/Layout/Layout.tsx
+++ frontend/src/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Footer } from "../components/Footer/Footer"
 
 export const Layout = () => {
     return (
-        <div className="max-w-341.5  mx-auto">
+        <div className="max-w-341.5 max-h-screen mx-auto">
             <div className="bg-[#F9F9F9]  drop-shadow-[#000029]">
                 <Header />
                 <Outlet />
